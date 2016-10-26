9fc85855196fb150c8fcfee6019bef6f0409d12e
JonMercer <mercer.jon@gmail.com>
2016-10-26 15:04:41 -0700

    Added 4 new algo questions

---
+++ java/src/coding_questions/MSwirl.java
@@ -0,0 +1,9 @@
+package coding_questions;
+
+/**
+ * Created by Odin on 2016-10-26.
+ */
+//Given a 2D array, how many steps until the two swirls meet?
+public interface MSwirl {
+    int swirl(char[][] arr);
+}
