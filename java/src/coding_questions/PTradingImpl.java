241a71f52ddef26f110072ea714ba74d0dfdae67
JonMercer <mercer.jon@gmail.com>
2016-11-03 19:58:06 -0700

    Set up PTrating

---
+++ java/src/coding_questions/PTradingImpl.java
@@ -0,0 +1,11 @@
+package coding_questions;
+
+/**
+ * Created by Odin on 2016-11-03.
+ */
+public class PTradingImpl implements PTrading {
+    @Override
+    public String detect(String str) {
+        return null;
+    }
+}
