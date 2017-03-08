e2470648e5557973d7093f03655fdde038442f75
JonMercer <mercer.jon@gmail.com>
2017-03-08 10:57:24 -0800

    SumResults

---
+++ java/src/coding_questions/HSumResults.java
@@ -0,0 +1,13 @@
+package coding_questions;
+
+/**
+ * Created by Odin on 2017-03-07.
+ */
+public interface HSumResults {
+
+
+    //find if a sum of two elements in numbers equals target
+    //throw illegal argument if numbers is less than one
+    //assume input is not sorted and can be negative
+    boolean targetInArray(int[] numbers, int target);
+}
